063c09b400a184b6f8840a1bd4726ffdc072b105
drad <robertjdstoddard@gmail.com>
2021-09-01 15:22:37 -0600

    finished api routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,18 @@
 const router = require('express').Router()
 const db = require('../../db/db.json')
+const fs = require('fs')
 
 router.get('/notes', (req, res) => {
-
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    res.json(data)
 })
 
 router.post('/notes', ({body}, res) =>{
-
+    let data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"))
+    
+    data.push(body)
+    fs.writeFileSync("./db/db.json", JSON.stringify(data))
+    res.json(data)
 })
 
 module.exports = router
